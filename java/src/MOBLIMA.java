cce1ea8ae113e609149096f338a39399a81ba5bf
lesfgoh <107750978+lesfgoh@users.noreply.github.com>
2022-11-12 13:28:21 +0800

    Update MOBLIMA.java
    Adding JavaDocs

--- java/src/MOBLIMA.java
+++ java/src/MOBLIMA.java
@@ -7,17 +7,44 @@ import view.MainMenuView;
 
 import java.util.Scanner;
 
+/**
+ * The Class MOBLIMA, main class and primary access point for users.
+ */
+
 public class MOBLIMA {
+	
+	/** The admin controller. */
+	
     private static AdminController adminController = new AdminController();
+    
+    /** The movie controller. */
+    
     private static MovieController movieController = new MovieController();
+    
+    /** The booking controller. */
+    
     private static BookingController bookingController = new BookingController();
+    
+    /** The cinema controller. */
+    
     private static CinemaController cinemaController = new CinemaController();
+    
+    /** The price controller. */
+    
     private static PriceController priceController = new PriceController();
+    
+    /** Generating the new menu. */
+    
     private static MainMenuView mainMenuView = new MainMenuView();
+    
+    /**
+     * Main function that gives us the IO menu.
+     *
+     * @param args the user input
+     */
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         mainMenuView.mainMenu(sc, adminController, movieController, cinemaController, bookingController, priceController);
     }
 }
-
